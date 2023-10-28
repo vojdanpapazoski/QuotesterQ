b18276c0b4a3882a127bdcbcb8d8be1d78187c27
vojdanpapazoski <vojdan_rage@yahoo.com>
2023-10-28 21:08:06 +0200

    setting up...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [advice, setAdvice] = useState('');
+
+  useEffect(() => {
+    console.log('Welcome!');
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = () => {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then((res) => {
+        const { advice } = res.data.slip; 
+        setAdvice(advice);
+        console.log(advice);
+      })
+      .catch((err) => {
+        console.log('Error during fetching!', err);
+      });
+  };
+
+  return <div className='container'>
+    <div className='main'>
+    <div className='quote'>{advice}</div>
+</div>
+</div>
 }
 
 export default App;
